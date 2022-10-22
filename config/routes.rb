047b9e32b060599bebfba1fff22184c3ca686b4e
prostokeks <tov.camz@gmail.com>
2022-10-22 13:30:15 +0300

    create devise user, create views: _header, _sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, controllers: {registrations: "users/registrations",
+                                   sessions: "users/sessions"}
+
+  root "static_pages#index"
+
 end
